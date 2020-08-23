2f473ef2781ec08f8b43da84640fa270426f0e6d
asarinn <mproemsey@gmail.com>
2020-08-22 23:16:02 -0500

    updated to fit ares character

--- main_window.py
+++ main_window.py
@@ -18,37 +18,25 @@ class MainWindow(QMainWindow):
         self.configuration = json.load(open("configuration.json", "r"))
 
         # Class variables to hold state of check boxes
-        self.two_handed_enabled = True
         self.inspire_courage_enabled = False
         self.haste_enabled = False
         self.raging_enabled = False
-        self.power_attack_enabled = False
-        self.enlarged_enabled = False
-        self.surprise_accuracy_enabled = False
-        self.powerful_blow_enabled = False
+        self.charging_enabled = False
         self.flanking_enabled = False
-        self.deadly_juggernaut_enabled = False
+        self.water_subtype_enabled = False
+        self.sneak_attack_enabled = False
 
         # Connections to toggle state on check box click
-        self.ui.two_handed_check_box.clicked.connect(self.two_handed_toggled)
         self.ui.inspire_courage_check_box.clicked.connect(self.inspire_courage_toggled)
         self.ui.haste_check_box.clicked.connect(self.haste_toggled)
         self.ui.raging_check_box.clicked.connect(self.raging_toggled)
-        self.ui.power_attack_check_box.clicked.connect(self.power_attack_toggled)
-        self.ui.enlarged_check_box.clicked.connect(self.enlarged_toggled)
-        self.ui.surprise_accuracy_check_box.clicked.connect(self.surprise_accuracy_toggled)
-        self.ui.powerful_blow_check_box.clicked.connect(self.powerful_blow_toggled)
+        self.ui.charging_check_box.clicked.connect(self.charging_toggled)
         self.ui.flanking_bonus_check_box.clicked.connect(self.flanking_toggled)
-        self.ui.deadly_juggernaut_check_box.clicked.connect(self.deadly_juggernaut_toggled)
-
-        # Auto update when spin box toggled
-        self.ui.kills_spin_box.valueChanged.connect(self.update_output)
+        self.ui.water_subtype_check_box.clicked.connect(self.water_subtype_toggled)
+        self.ui.flanking_bonus_check_box.clicked.connect(self.flanking_toggled)
+        self.ui.sneak_attack_check_box.clicked.connect(self.sneak_attack_toggled)
 
         # Initialize output with initial settings
-        self.update_output()
-
-    def two_handed_toggled(self, state):
-        self.two_handed_enabled = state
         self.update_output()
 
     def inspire_courage_toggled(self, state):
@@ -63,43 +51,34 @@ class MainWindow(QMainWindow):
         self.raging_enabled = state
         self.update_output()
 
-    def power_attack_toggled(self, state):
-        self.power_attack_enabled = state
+    def charging_toggled(self, state):
+        self.charging_enabled = state
         self.update_output()
 
-    def enlarged_toggled(self, state):
-        self.enlarged_enabled = state
-        self.update_output()
-
-    def surprise_accuracy_toggled(self, state):
-        self.surprise_accuracy_enabled = state
-        self.update_output()
-
-    def powerful_blow_toggled(self, state):
-        self.powerful_blow_enabled = state
+    def water_subtype_toggled(self, state):
+        self.water_subtype_enabled = state
         self.update_output()
 
     def flanking_toggled(self, state):
         self.flanking_enabled = state
         self.update_output()
 
-    def deadly_juggernaut_toggled(self, state):
-        self.deadly_juggernaut_enabled = state
+    def sneak_attack_toggled(self, state):
+        self.sneak_attack_enabled = state
         self.update_output()
 
     def update_output(self):
-        # Calculate strength bonus
-        strength = self.configuration['STR']
+        raw_dex = self.configuration['DEX']
         if self.raging_enabled:
-            strength += self.configuration['RAGE_STR_BONUS']
-        if self.enlarged_enabled:
-            strength += self.configuration['ENLARGE_STR_BONUS']
-        effective_strength_bonus = int((strength - 10) / 2)
+            raw_dex += self.configuration['RAGE_DEX_BONUS']
+
+        bonus_dex = int((raw_dex - 10) / 2)
+
+        # Calculate Attack Bonus
+        attack_bonus = self.calculate_attack_bonus(bonus_dex)
 
         # Calculate number of attacks
         num_attacks = 1 + int((self.configuration['BAB'] - 1) / 5)
-
-        attack_bonus = self.calculate_attack_bonus(effective_strength_bonus)
 
         attack_text = f' Attack Bonus: +{attack_bonus}'
         if self.haste_enabled:
@@ -108,15 +87,20 @@ class MainWindow(QMainWindow):
             attack_text = attack_text + f'/{attack_bonus - (5 * (i + 1))}'
         self.ui.attack_bonus_label.setText(attack_text)
 
-        die, damage = self.calculate_damage(effective_strength_bonus)
-        self.ui.damage_label.setText(f'Damage: {die[0]}d{die[1]} + {damage}')
+        # Calculate Damage Bonus
+        damage = self.calculate_damage(bonus_dex)
+
+        # Calculate Dice
+        dice, crit_dice = self.calculate_dice()
+
+        self.ui.damage_label.setText(f'Damage: {dice} + {damage}')
 
         crit_mod = self.configuration["WEAPON_CRITICAL_MOD"]
         self.ui.crit_damage_label.setText(
-            f'Critical Damage: {crit_mod * die[0]}d{die[1]} + {crit_mod * damage}')
+            f'Critical Damage: {crit_dice} + {crit_mod * damage}')
 
-    def calculate_attack_bonus(self, effective_strength_bonus):
-        attack_bonus = self.configuration['BAB'] + self.configuration['WEAPON_BONUS'] + effective_strength_bonus
+    def calculate_attack_bonus(self, dex):
+        attack_bonus = self.configuration['BAB'] + self.configuration['WEAPON_BONUS'] + dex
 
         if self.inspire_courage_enabled:
             attack_bonus += self.configuration['INSPIRE']
@@ -124,49 +108,43 @@ class MainWindow(QMainWindow):
         if self.haste_enabled:
             attack_bonus += self.configuration['HASTE']
 
-        if self.power_attack_enabled:
-            attack_bonus += self.configuration['POWER_ATTACK_ATTACK']
-
-        if self.enlarged_enabled:
-            attack_bonus += self.configuration['ENLARGE']
-
-        if self.surprise_accuracy_enabled:
-            attack_bonus += self.configuration['SURPRISE_ACCURACY']
+        if self.charging_enabled:
+            attack_bonus += self.configuration['CHARGE']
 
         if self.flanking_enabled:
             attack_bonus += self.configuration['FLANKING']
 
-        if self.deadly_juggernaut_enabled:
-            attack_bonus += self.ui.kills_spin_box.value()
+        if self.sneak_attack_enabled:
+            attack_bonus += self.configuration['SNEAK_ATTACK_BONUS']
 
         return attack_bonus
 
-    def calculate_damage(self, effective_strength_bonus):
-        damage = self.configuration['WEAPON_BONUS']
-
-        strength_damage = effective_strength_bonus
-        if self.two_handed_enabled:
-            strength_damage = int(strength_damage * self.configuration['TWO_HANDED_MULTI'])
-        damage += strength_damage
-
-        if self.powerful_blow_enabled:
-            damage += self.configuration['POWERFUL_BLOW']
-
-        power_attack_damage = 6
-        if self.two_handed_enabled:
-            power_attack_damage = int(power_attack_damage * self.configuration['TWO_HANDED_MULTI'])
-        if self.power_attack_enabled:
-            damage += power_attack_damage
+    def calculate_damage(self, dex):
+        damage = self.configuration['WEAPON_BONUS'] + dex
 
         if self.inspire_courage_enabled:
             damage += self.configuration['INSPIRE']
 
-        if self.deadly_juggernaut_enabled:
-            damage += self.ui.kills_spin_box.value()
+        return damage
 
-        if self.enlarged_enabled:
-            die = self.configuration['ENLARGE_DAMAGE_DIE']
-        else:
-            die = self.configuration['DAMAGE_DIE']
+    def calculate_dice(self):
+        weapon_dice = self.configuration['DAMAGE_DIE']
 
-        return die, damage
+        crit_multiplier = self.configuration['WEAPON_CRITICAL_MOD']
+
+        sneak_dice = self.configuration['SNEAK_DAMAGE_DIE']
+
+        if self.water_subtype_enabled and not self.sneak_attack_enabled:
+            dice = f'{weapon_dice[0]}d{weapon_dice[1]}'
+            crit_dice = f'{crit_multiplier * weapon_dice[0] + 3}d{weapon_dice[1]}'
+        elif self.sneak_attack_enabled and not self.water_subtype_enabled:
+            dice = f'{weapon_dice[0]+sneak_dice[0]}d{weapon_dice[1]}'
+            crit_dice = f'{crit_multiplier * weapon_dice[0]+sneak_dice[0]}d{weapon_dice[1]}'
+        elif not self.sneak_attack_enabled and not self.water_subtype_enabled:
+            dice = f'{weapon_dice[0]}d{weapon_dice[1]}'
+            crit_dice = f'{crit_multiplier * weapon_dice[0]}d{weapon_dice[1]}'
+        elif self.sneak_attack_enabled and self.water_subtype_enabled:
+            dice = f'{weapon_dice[0] + sneak_dice[0]}d{weapon_dice[1]}'
+            crit_dice = f'{crit_multiplier * weapon_dice[0] + 3 + sneak_dice[0]}d{weapon_dice[1]}'
+
+        return dice, crit_dice
